717eab98c005167a40706efa657a009914a7090b
yapeansa <yago.pereira@estudante.ufjf.br>
2024-11-09 14:23:33 -0300

    Refatorando o código: novos prognósticos a cada nova execução do programa.

--- main.c
+++ main.c
@@ -6,11 +6,15 @@ FILE *bd;
 
 void leitura(int n)
 {
+    bd = fopen("arquivo.txt", "r");
+
     for (int i = 1; i <= n; i++) {
         int x_1, x_2, x_3, x_4, x_5, x_6;
         fscanf(bd, "%d %d %d %d %d %d", &x_1, &x_2, &x_3, &x_4, &x_5, &x_6);
         printf("Prognóstico: %d %d %d %d %d %d\n", x_1, x_2, x_3, x_4, x_5, x_6);
     }
+
+    fclose(bd);
 }
 
 void grava(int x_1, int x_2, int x_3, int x_4, int x_5, int x_6)
@@ -34,16 +38,15 @@ int main(void)
     int i, N;
     srand(time(NULL));
 
+    bd = fopen("arquivo.txt", "w");
+
     printf("Por favor, digite o número de prognósticos desejado: ");
     scanf("%d", &N);
 
     for (i = 1; i <= N; i++)
         grava(Random(1, 60), Random(1, 60), Random(1, 60), Random(1, 60), Random(1, 60), Random(1, 60));
 
-    bd = fopen("arquivo.txt", "r");
-
     leitura(N);
 
-    fclose(bd);
     return 0;
 }
